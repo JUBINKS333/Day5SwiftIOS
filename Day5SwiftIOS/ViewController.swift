b3408f8d6f7d5269d838dd163802274ce9cf73eb
MacStudent <macstudent@126-imac29.local>
2019-03-08 19:23:20 -0500

    date picker ,picker view

--- Day5SwiftIOS/ViewController.swift
+++ Day5SwiftIOS/ViewController.swift
@@ -8,13 +8,52 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UIPickerViewDelegate,UIPickerViewDataSource {
+   
+   
+    
+    
+    @IBOutlet weak var picker: UIPickerView!
+    
+    
+    @IBOutlet weak var lblCountryName: UILabel!
+    
+    let country = ["usa","canada","india","africa","china","japan","argentina","brazil","russia","dubai"]
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.picker.delegate=self
+        self.picker.dataSource=self
     }
 
 
+    @IBAction func birthDatePicker(_ sender: UIDatePicker) {
+        
+      print( sender.date)
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        
+        return self.country.count
+        
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        
+        //    return "comp: \(component) row \(row)"
+        
+        return self.country[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+          lblCountryName.text=self.country[row]
+    }
 }
 
